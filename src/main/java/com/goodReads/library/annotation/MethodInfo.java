bceff1ccea7e58cd1f9352a201ac71cabedb6581
SaiSandeepGollapudi <gsandeep2406@gmail.com>
2024-06-03 20:58:03 -0700

    LibraryApplication

---
+++ src/main/java/com/goodReads/library/annotation/MethodInfo.java
@@ -0,0 +1,16 @@
+package com.goodReads.library.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface MethodInfo {
+
+    String author();
+
+    String description();
+}
--- src/main/java/com/goodReads/library/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/goodReads/library/service/impl/ReviewServiceImpl.java
@@ -1,7 +1,9 @@
 package com.goodReads.library.service.impl;
 
+
 import com.goodReads.library.Repositry.BookRepository;
 import com.goodReads.library.Repositry.ReviewRepository;
+import com.goodReads.library.annotation.MethodInfo;
 import com.goodReads.library.domain.Book;
 import com.goodReads.library.domain.Review;
 import com.goodReads.library.service.ReviewService;
@@ -10,6 +12,52 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Override
+    @MethodInfo(author = "sai", description = "add review")
+    public void addReview(Integer bookId, Review review) {
+        // Retrieve the book by its ID
+        Optional<Book> bookOptional = bookRepository.findById(bookId);// The result is wrapped in an Optional to handle cases where the book may not exist.
+        if (bookOptional.isPresent()) {
+            Book book = bookOptional.get();
+
+            // Add the review to the book's review list
+            book.getReviewList().add(review);
+
+            // Update the book's rating
+            updateBookRating(book);//is called to recalculate and update the book's rating based on its reviews.
+
+            // Save the review
+            review.setBook(book);//the review is associated with the book
+            reviewRepository.save(review);//  saved to the database using reviewRepository.save(review).
+        } else {
+            throw new IllegalArgumentException("Book with ID " + bookId + " not found");
+        }
+    }
+
+    private void updateBookRating(Book book) {
+        List<Review> reviewList = book.getReviewList();
+        if (!reviewList.isEmpty()) {
+            double totalRating = 0.0;
+            for (Review review : reviewList) {//it calculates the total rating by summing up the ratings of all the reviews.
+                totalRating += review.getRating();
+            }
+            double averageRating = totalRating / reviewList.size();// it calculates the average rating by dividing the total rating by the number of reviews.
+            book.setRating(averageRating);//The average rating is set to the book's rating attribute
+            bookRepository.save(book);//the updated book object is saved to the database
+        }
+    }
+
+}
 
 //@Service
 //public class ReviewServiceImpl implements ReviewService {
@@ -50,50 +98,7 @@ import java.util.Optional;
 //    }
 //    }
 
-@Service
-public class ReviewServiceImpl implements ReviewService {
 
-    @Autowired
-    ReviewRepository reviewRepository;
-
-    @Autowired
-    BookRepository bookRepository;
-
-    @Override
-    public void addReview(Integer bookId, Review review) {
-        // Retrieve the book by its ID
-        Optional<Book> bookOptional = bookRepository.findById(bookId);// The result is wrapped in an Optional to handle cases where the book may not exist.
-        if (bookOptional.isPresent()) {
-            Book book = bookOptional.get();
-
-            // Add the review to the book's review list
-            book.getReviewList().add(review);
-
-            // Update the book's rating
-            updateBookRating(book);//is called to recalculate and update the book's rating based on its reviews.
-
-            // Save the review
-            review.setBook(book);//the review is associated with the book
-            reviewRepository.save(review);//  saved to the database using reviewRepository.save(review).
-        } else {
-            throw new IllegalArgumentException("Book with ID " + bookId + " not found");
-        }
-    }
-
-    private void updateBookRating(Book book) {
-        List<Review> reviewList = book.getReviewList();
-        if (!reviewList.isEmpty()) {
-            double totalRating = 0.0;
-            for (Review review : reviewList) {//it calculates the total rating by summing up the ratings of all the reviews.
-                totalRating += review.getRating();
-            }
-            double averageRating = totalRating / reviewList.size();// it calculates the average rating by dividing the total rating by the number of reviews.
-            book.setRating(averageRating);//The average rating is set to the book's rating attribute
-            bookRepository.save(book);//the updated book object is saved to the database
-        }
-    }
-
-}
 ///Book book1= bookMap.get(bookId);
 //        List<Review> reviewList= book1.getReviewList();
 //        List<Double> ratings= new ArrayList<Double>();
